e83e94a1a060178f7ae11705b3f9ab4a445f06f1
Martin Bäumer <martin.baeumer@gmail.com>
2018-02-04 17:14:24 +0100

    added part1 and part2

---
+++ part2/src/main/java/se/mbaeumer/githubhero/part2/SpringBootLabGithubHeroApplication.java
@@ -0,0 +1,11 @@
+package se.mbaeumer.githubhero.part2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootLabGithubHeroApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootLabGithubHeroApplication.class, args);
+	}
+}
---
+++ part2/src/main/java/se/mbaeumer/githubhero/part2/controllers/WelcomeController.java
@@ -0,0 +1,13 @@
+package se.mbaeumer.githubhero.part2.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeController {
+    @RequestMapping(value="/welcome", method= RequestMethod.GET)
+    public String welcome(){
+        return "Welcome";
+    }
+}
