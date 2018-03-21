111c11a787aafbfc8bf263ea12c644bbbb2a1a26
Martin Bäumer <martin.baeumer@gmail.com>
2018-03-21 22:47:30 +0100

    added exaple for testing the RestTemplate

---
+++ part4/src/main/java/se/mbaeumer/githubhero/part3/services/User.java
@@ -0,0 +1,15 @@
+package se.mbaeumer.githubhero.part3.services;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+    private String login;
+    private int id;
+    private String avatarUrl;
+    private String url;
+}
